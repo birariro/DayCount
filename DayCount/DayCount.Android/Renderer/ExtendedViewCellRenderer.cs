e3e36c0ad54351e57d0d944adc17acf5377af746
k4keye <vps32@naver.com>
2020-10-26 18:47:27 +0900

    리스트뷰 컬러 제거

---
+++ DayCount/DayCount.Android/Renderer/ExtendedViewCellRenderer.cs
@@ -0,0 +1,56 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Views;
+using DayCount.Droid.Renderer;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using DayCount.Renderer;
+
+[assembly: ExportRenderer(typeof(ExtendedViewCell), typeof(ExtendedViewCellRenderer))]
+
+namespace DayCount.Droid.Renderer
+{
+    public class ExtendedViewCellRenderer : ViewCellRenderer
+    {
+        private Android.Views.View cellCoreView;
+        private Drawable unSelectedBackground;
+        private bool isSelected;
+
+        protected override Android.Views.View GetCellCore(
+            Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
+        {
+            this.cellCoreView = base.GetCellCore(item, convertView, parent, context);
+
+
+
+            this.isSelected = false;
+            this.unSelectedBackground = cellCoreView.Background;
+
+            return cellCoreView;
+        }
+
+        protected override void OnCellPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            base.OnCellPropertyChanged(sender, args);
+
+            if (args.PropertyName == "IsSelected")
+            {
+                this.isSelected = !this.isSelected;
+
+                if (this.isSelected)
+                {
+                    var extendedViewCell = sender as ExtendedViewCell;
+                    this.cellCoreView.SetBackgroundColor(extendedViewCell.SelectedBackgroundColor.ToAndroid());
+                }
+                else
+                {
+                    this.cellCoreView.SetBackground(this.unSelectedBackground);
+                }
+            }
+        }
+    }
+}
+
+
---
+++ DayCount/DayCount.iOS/Renderer/ExtendedViewCellRenderer.cs
@@ -0,0 +1,24 @@
+﻿using DayCount.iOS.Renderer;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using DayCount.Renderer
+[assembly: ExportRenderer(typeof(ExtendedViewCell), typeof(ExtendedViewCellRenderer))]
+
+namespace DayCount.iOS.Renderer
+{
+    class ExtendedViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+            var view = item as ExtendedViewCell;
+            cell.SelectedBackgroundView = new UIView
+            {
+                BackgroundColor = view.SelectedBackgroundColor.ToUIColor(),
+            };
+
+            return cell;
+        }
+    }
+}
